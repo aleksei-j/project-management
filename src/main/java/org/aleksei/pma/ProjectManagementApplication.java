4ea4c5fa97f7f812c64b1e290c18c92909ea8387
aleksei-j <rololik.1@gmail.com>
2021-01-31 15:47:42 +0200

    Initial commit

---
+++ src/main/java/org/aleksei/pma/ProjectManagementApplication.java
@@ -0,0 +1,14 @@
+package org.aleksei.pma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProjectManagementApplication.class, args);
+	}
+
+}
